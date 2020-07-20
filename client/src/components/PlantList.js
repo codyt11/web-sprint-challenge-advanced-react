04a996d60c2c715d888f29887588afbd75101c59
Codyt11 <Codyt11@hotmail.com>
2020-07-19 21:16:43 -0500

    MVP

--- client/src/components/PlantList.js
+++ client/src/components/PlantList.js
@@ -3,6 +3,21 @@ import axios from "axios";
 
 export default class PlantList extends Component {
   // add state with a property called "plants" - initialize as an empty array
+  constructor(){
+    super()
+    this.state = {
+      plants:[]
+    }
+  }
+
+  componentDidMount(){
+    axios.get('http://localhost:3333/plants')
+    .then(response => {
+      console.log(response.data.plantsData)
+    this.setState({plants: response.data.plantsData})
+    })
+
+  }
 
   // when the component mounts:
   //   - fetch data from the server endpoint - http://localhost:3333/plants
@@ -12,7 +27,7 @@ export default class PlantList extends Component {
   render() {
     return (
       <main className="plant-list">
-        {this.state?.plants?.map((plant) => (
+        {this.state.plants.map(plant => (
           <div className="plant-card" key={plant.id}>
             <img className="plant-image" src={plant.img} alt={plant.name} />
             <div className="plant-details">
